e637c66a4ad3fc8b9fe0d49d8256b95f770c6772
namluan <namguerin@gmail.com>
2023-10-20 11:28:14 +0200

    initial commit

---
+++ mission2SicilyLines/DAL/ConnexionSql.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace mission2SicilyLines.DAL
+{
+        public class ConnexionSql
+
+        {
+
+            // Variable locale pour stocker une référence vers l'instance
+            private static ConnexionSql connection = null;
+
+            private MySqlConnection mySqlCn;
+            private static readonly object mylock = new object();
+
+
+
+            private ConnexionSql(string unProvider, string uneDataBase, string unUid, string
+            unMdp)
+            {
+
+                try
+                {
+                    string connString;
+                    connString = "SERVER=" + unProvider + ";" + "DATABASE=" +
+                    uneDataBase + ";" + "UID=" + unUid + ";" + "PASSWORD=" + unMdp + ";";
+                    try
+                    {
+                        mySqlCn = new MySqlConnection(connString);
+                    }
+                    catch (Exception emp)
+                    {
+                        throw (emp);
+                    }
+                }
+                catch (Exception emp)
+                {
+                    throw (emp);
+                }
+            }
+
+            /**
+            * méthode de création d'une instance de connexion si elle n'existe pas (singleton)
+            */
+            public static ConnexionSql getInstance(string unProvider, string uneDataBase, string
+            unUid, string unMdp)
+            {
+                lock ((mylock))
+                {
+
+                    try
+                    {
+
+                        if (null == connection)
+                        { // Premier appel
+                            connection = new ConnexionSql(unProvider, uneDataBase, unUid,
+                           unMdp);
+                        }
+                    }
+                    catch (Exception emp)
+                    {
+
+                        throw (emp);
+                    }
+                    return connection;
+                }
+            }
+
+            /**
+            * Ouverture de la connexion
+            */
+            public void openConnection()
+            {
+                try
+                {
+                    mySqlCn.Open();
+                }
+                catch (Exception emp)
+                {
+                    throw (emp);
+                }
+            }
+            /**
+            * Fermeture de la connexion
+            */
+            public void closeConnection()
+            {
+                mySqlCn.Close();
+            }
+            /**
+            * Exécution d'une requête
+            */
+            public MySqlCommand reqExec(string req)
+            {
+                MySqlCommand mysqlCom = new MySqlCommand(req, this.mySqlCn);
+                return (mysqlCom);
+            }
+
+        }
+    }
--- mission2SicilyLines/Form1.Designer.cs
+++ mission2SicilyLines/Form1.Designer.cs
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBoxSecteur = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBoxSecteur
+            // 
+            this.listBoxSecteur.FormattingEnabled = true;
+            this.listBoxSecteur.ItemHeight = 20;
+            this.listBoxSecteur.Location = new System.Drawing.Point(115, 79);
+            this.listBoxSecteur.Name = "listBoxSecteur";
+            this.listBoxSecteur.Size = new System.Drawing.Size(553, 284);
+            this.listBoxSecteur.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBoxSecteur);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBoxSecteur;
     }
 }
 
