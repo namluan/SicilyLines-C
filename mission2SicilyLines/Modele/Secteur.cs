49af9e248462191a0ec6ac25d3290b89f4da6154
namluan <namguerin@gmail.com>
2023-10-20 11:47:50 +0200

    client.cs & secteur.cs creation

---
+++ mission2SicilyLines/Modele/Secteur.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mission2SicilyLines.Modele
+{
+    public class Secteur
+    {
+        private int id;
+        private string libelle;
+    }
+}
